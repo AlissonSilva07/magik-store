a7405256332bc9fa52658f0a56cb04cae3a55579
Alisson Rego <alissondroid07@gmail.com>
2024-06-21 16:36:47 -0300

    feat: criar o componente Hero para tablet e desktop

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,10 +5,10 @@ export interface IHeroProps {
 
 export function Hero (props: IHeroProps) {
   return (
-    <div className='flex flex-col items-center gap-4'>
-      <img src={HeroImg} alt="A woman carrying bags and pointing to her left side." className='border-b border-preto' />
-      <div className='flex flex-col items-center gap-4'>
-        <p className='font-black text-5xl text-center'>WANNA KNOW WHAT’S SO <span className='text-roxo'>MAGIK!</span> ABOUT SHOPPING FOR STUFF?</p>
+    <div className='flex flex-col lg:flex-row items-center gap-4'>
+      <img src={HeroImg} alt="A woman carrying bags and pointing to her left side." className='w-full h-[427px] md:h-[449px] lg:w-1/2 lg:h-[800px] object-cover border-b border-preto' />
+      <div className='flex p-4 md:p-8 flex-col items-center lg:items-start gap-4'>
+        <p className='font-black text-5xl md:text-[64px] text-center lg:text-start'>WANNA KNOW WHAT’S SO <span className='text-roxo'>MAGIK!</span> ABOUT SHOPPING FOR STUFF?</p>
         <p>You are one click away from finding out. Hop in!</p>
         <button className='py-2 px-3 bg-white flex items-center gap-2 rounded-full border border-preto'>
             <p>See Products</p>
