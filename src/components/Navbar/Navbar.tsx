d6d195e8dfa46b198b67d5658a7cdb1225448f8f
Alisson Rego <alissondroid07@gmail.com>
2024-07-10 16:51:55 -0300

    style: efeitos de hover nos links da navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,20 +11,20 @@ export function Navbar () {
     <nav className='fixed top-0 right-0 left-0 h-20 bg-white px-4 md:px-8 bg-branco flex items-center justify-between border-b border-cinza-border/20 z-40'>
       <p className='text-2xl font-black'>MAGIK!</p>
 
-      <div className='flex items-center gap-8'>
+      <div className='h-full flex items-center gap-8'>
         <button onClick={() => setShowSearch(true)} className='hidden md:flex'>
           <Search className='w-[18px] rounded-full' />
         </button>
         <button onClick={() => setShowMenu(true)} className='lg:hidden'>
           <MenuIcon />
         </button>
-        <div className='hidden lg:flex items-center gap-8'>
-          <a href="#" className='hover:font-medium'>Home</a>
-          <a href="#" className='hover:font-medium'>Products</a>
-          <a href="#" className='hover:font-medium'>Cart (0)</a>
+        <div className='hidden h-full lg:flex items-center gap-8'>
+          <a href="#" className='h-full flex items-center hover:font-bold hover:border-b-4 hover:border-roxo'>Home</a>
+          <a href="#" className='h-full flex items-center hover:font-bold hover:border-b-4 hover:border-roxo'>Products</a>
+          <a href="#" className='h-full flex items-center hover:font-bold hover:border-b-4 hover:border-roxo'>Cart (0)</a>
         </div>
-        <div className='hidden lg:flex w-[1px] h-8 bg-preto'></div>
-        <a href="#" className='hidden lg:flex hover:font-medium'>Login / Register</a>
+        <div className='hidden lg:flex w-[1px] h-8 bg-cinza-border/20'></div>
+        <a href="#" className='hidden lg:flex hover:font-bold'>Login / Register</a>
       </div>
 
       <Menu showMenu={showMenu} onCloseMenu={() => setShowMenu(false)} />
