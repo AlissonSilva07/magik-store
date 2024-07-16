b5d7ee293f8197d72f20eaabdebb5cf1719f62f8
Alisson Rego <alissondroid07@gmail.com>
2024-07-16 16:16:37 -0300

    feat: adicionar lógica e layout de contador de filtros desktop

--- src/components/Products/SidebarFilters.tsx
+++ src/components/Products/SidebarFilters.tsx
@@ -11,13 +11,13 @@ export interface ISidebarFiltersProps {
 
 export function SidebarFilters() {
   const { categories } = useApi()
-  const { updateFilter,
-    removeFilter } = useContext(ProductContext)
+  const { filter, filterCounter, updateFilter, removeFilter } = useContext(ProductContext)
 
-  const [categoryFilter, setCategoryFilter] = useState<string>('')
-  const [sortingFilter, setSortingFilter] = useState<string>('')
-  const [maxFilter, setMaxFilter] = useState<string | number | readonly string[] | undefined>(undefined)
-  const [minFilter, setMinFilter] = useState<string | number | readonly string[] | undefined>(undefined)
+  const [categoryFilter, setCategoryFilter] = useState<string>(filter ? filter.category : '')
+  const [sortingFilter, setSortingFilter] = useState<string>(filter ? filter.sort_by : '')
+  const [maxFilter, setMaxFilter] = useState<string | number | readonly string[] | undefined>(filter ? filter.price.max : undefined)
+  const [minFilter, setMinFilter] = useState<string | number | readonly string[] | undefined>(filter ? filter.price.min : undefined)
+
 
   const setFilters = () => {
     updateFilter({
@@ -45,11 +45,11 @@ export function SidebarFilters() {
           <div className='flex flex-col gap-4'>
             <div className='w-full flex flex-col gap-4'>
               <p className='font-bold'>Category</p>
-              <SelectCategory categories={categories} setCategoryFilter={setCategoryFilter} />
+              <SelectCategory categories={categories} filterCategory={filter.category} setCategoryFilter={setCategoryFilter} />
             </div>
             <div className='flex flex-col gap-4'>
               <p className='font-bold'>Sort By</p>
-              <SelectSorting sorting={sorting} setSortingFilter={setSortingFilter} />
+              <SelectSorting sorting={sorting} filterSort={filter.sort_by} setSortingFilter={setSortingFilter} />
             </div>
             <div className='flex flex-col gap-4'>
               <p className='font-bold'>Price Range</p>
