13b955f6c1075af2eb2440d1a3bcb18338b1b5e2
Alisson Rego <alissondroid07@gmail.com>
2024-07-27 22:55:34 -0300

    style: mudanças no layout das categorias da tela home

--- src/components/Home/Categories/CategoryButton.tsx
+++ src/components/Home/Categories/CategoryButton.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import ButtonGo from '../../../assets/Categories/Button Go.png'
 import Electronics from '../../../assets/Categories/electronics.jpg'
 import Jewelry from '../../../assets/Categories/jewelery.jpg'
 import MensClothing from '../../../assets/Categories/mens clothing.jpg'
@@ -33,12 +32,9 @@ export function CategoryButton ({ title }: ICategoryButtonProps) {
   }
 
   return (
-    <div onClick={goToCategory} className='group relative w-full p-2.5 flex flex-col items-center justify-center border border-branco hover:border-preto rounded-lg z-30 cursor-pointer'>
+    <div onClick={goToCategory} className='group relative w-full p-2.5 flex flex-col items-center justify-center border border-branco hover:border-cinza-border/20 rounded-lg z-30 cursor-pointer'>
       <div className='absolute flex items-center gap-4 z-30'>
-        <p className='text-2xl font-bold text-branco [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]'>{title.toLocaleUpperCase()}</p>
-        <button className='hidden group-hover:block'>
-            <img src={ButtonGo} alt="Go" />
-        </button>
+        <p className='text-2xl font-bold text-branco hover:[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]'>{title.toLocaleUpperCase()}</p>
       </div>
 
       <img src={
@@ -47,7 +43,7 @@ export function CategoryButton ({ title }: ICategoryButtonProps) {
         title === "men's clothing" ? MensClothing :
         title === "women's clothing" ? WomensClothing :
         'null'
-      } alt={title} className='w-full h-[92px] object-cover md:h-[184px] lg:h-[504px] rounded-lg opacity-70' />
+      } alt={title} className='w-full h-[92px] object-cover md:h-[184px] lg:h-[504px] rounded-lg opacity-70 group-hover:brightness-50' />
     </div>
   );
 }
